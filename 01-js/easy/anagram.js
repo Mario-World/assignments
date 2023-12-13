36fba76feb41ed1e161cd773bd39456e8edbf252
PavanSai <81609557+Mario-World@users.noreply.github.com>
2023-12-13 12:12:08 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,10 @@
 */
 
 function isAnagram(str1, str2) {
+const cleanSrt1 = str1.replace(/\s/g, ' ').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, ' ').tpoLowerCase();
 
+  return cleanStr1.split('').sort().join('') === cleanStr2.split('').sort().join(''); 
 }
 
 module.exports = isAnagram;
